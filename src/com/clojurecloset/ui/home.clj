899cde93e04216a8ee871d2574ae51c4a3c8d114
John Oerter <john@johnoerter.me>
2024-02-04 16:21:08 -0500

    Update hero

--- src/com/clojurecloset/ui/home.clj
+++ src/com/clojurecloset/ui/home.clj
@@ -70,15 +70,15 @@
      "relative mx-auto flex max-w-3xl flex-col items-center px-6 py-32 text-center sm:py-64 lg:px-0"}
     [:h1
      {:class "text-4xl font-bold tracking-tight text-white lg:text-6xl"}
-     "New arrivals are here"]
+     "Welcome to Clojure Closet"]
     [:p
      {:class "mt-4 text-xl text-white"}
-     "The new arrivals have, well, newly arrived. Check out the latest options from our summer small-batch release while they&#39;re still in stock."]
+     "Support your favorite projects with the finest Clojure merchandise."]
     [:a
      {:href "#",
       :class
       "mt-8 inline-block rounded-md border border-transparent bg-white px-8 py-3 text-base font-medium text-gray-900 hover:bg-gray-100"}
-     "Shop New Arrivals"]]])
+     "Shop Now"]]])
 
 (def section-support-projects
   [:section
