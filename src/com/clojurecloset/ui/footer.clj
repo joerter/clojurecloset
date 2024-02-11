3b6cd531ac5105560aff48ccf7b140d334348715
John Oerter <john@johnoerter.me>
2024-02-10 23:36:05 -0500

    Simplify footer

--- src/com/clojurecloset/ui/footer.clj
+++ src/com/clojurecloset/ui/footer.clj
@@ -7,31 +7,6 @@
    [:div
     {:class "mx-auto max-w-7xl px-4 sm:px-6 lg:px-8"}
     [:div
-     {:class "py-20 xl:grid xl:grid-cols-3 xl:gap-8"}
-     [:div
-      {:class "grid grid-cols-2 gap-8 xl:col-span-2"}
-      [:div
-       {:class "space-y-12 md:grid md:grid-cols-2 md:gap-8 md:space-y-0"}
-       [:div
-        [:h3 {:class "text-sm font-medium text-white"} "Shop"]
-        [:ul
-         {:role "list", :class "mt-6 space-y-6"}
-         [:li
-          {:class "text-sm"}
-          [:a
-           {:href "#", :class "text-gray-300 hover:text-white"}
-           "Stickers"]]
-         [:li
-          {:class "text-sm"}
-          [:a
-           {:href "#", :class "text-gray-300 hover:text-white"}
-           "T-Shirts"]]
-         [:li
-          {:class "text-sm"}
-          [:a
-           {:href "#", :class "text-gray-300 hover:text-white"}
-           "Accessories"]]]]]]]
-    [:div
      {:class "border-t border-gray-800 py-10"}
      [:p
       {:class "text-sm text-gray-400"}
