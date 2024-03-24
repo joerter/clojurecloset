cfa62027d0a9175a49aa16b3580b453f27d60c0b
John Oerter <john@johnoerter.me>
2024-03-24 17:07:50 -0400

    Remove add to favorites

--- src/com/clojurecloset/ui/product.clj
+++ src/com/clojurecloset/ui/product.clj
@@ -78,23 +78,7 @@
        :class
        "flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full"}
       "Add to cart"]
-     [:button
-      {:type "button",
-       :class
-       "ml-4 flex items-center justify-center rounded-md px-3 py-3 text-gray-400 hover:bg-gray-100 hover:text-gray-500"}
-      [:svg
-       {:class "h-6 w-6 flex-shrink-0",
-        :fill "none",
-        :viewBox "0 0 24 24",
-        :stroke-width "1.5",
-        :stroke "currentColor",
-        :aria-hidden "true"}
-       [:path
-        {:stroke-linecap "round",
-         :stroke-linejoin "round",
-         :d
-         "M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"}]]
-      [:span {:class "sr-only"} "Add to favorites"]]]]])
+     ]]])
 
 (def details
   [:div
